ee3f76464a8e6f8e358ca4b13bb09a9285f39c4e
unknown <vladocrat@gmail.com>
2023-03-27 19:20:14 +0300

    added handle

---
+++ handle.cpp
@@ -0,0 +1,15 @@
+#include "handle.h"
+
+namespace WinUtils
+{
+	Handle::Handle(HANDLE h, std::function<void()> close = {}) 
+		: m_h(h), m_closeFunc(close) {}
+
+	Handle::~Handle()
+	{
+		if (m_closeFunc)
+		{
+			m_closeFunc();
+		}
+	}
+} // WinUtils
---
+++ handle.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <Windows.h>
+#include <functional>
+
+namespace WinUtils
+{
+	class Handle
+	{
+		Handle(HANDLE h, std::function<void()> close = {});
+		~Handle();
+
+		HANDLE* winHandle() { return &m_h; }
+
+	private:
+		HANDLE m_h = INVALID_HANDLE_VALUE;
+		std::function<void()> m_closeFunc = {};
+	};
+} // WinUtils
