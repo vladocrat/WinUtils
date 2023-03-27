bb1653483439f23bd2d8d2068511b35dd251b4df
unknown <vladocrat@gmail.com>
2023-03-27 22:02:30 +0300

    updated handled dtor

--- handle.cpp
+++ handle.cpp
@@ -1,27 +1,44 @@
 #include "handle.h"
 
+#include <iostream>
+
+#include "error.h"
+
 namespace WinUtils
 {
 	Handle::Handle() {}
 
-	Handle::Handle(HANDLE h, std::function<void()> close = {}) 
+	Handle::Handle(HANDLE h, std::function<void()> close)
 		: m_h(h), m_closeFunc(close) {}
 
 	Handle::~Handle()
 	{
+		if (m_h == INVALID_HANDLE_VALUE)
+		{
+			std::cerr << "handle is invalid" << std::endl;
+			return;
+		}
+
 		if (!CloseHandle(m_h))
 		{
+			std::cerr << "failed to close handle: " << lastErrorAsString() << std::endl;
+
 			if (m_closeFunc)
 			{
 				m_closeFunc();
 			}
+		}
+
+		if (m_closeFunc)
+		{
+			m_closeFunc();
 		}
 	}
 
 	void Handle::setFunc(std::function<void()> f)
 	{
 		m_closeFunc = f;
-	} 
+	}
 
 	void Handle::setWinHandle(HANDLE h)
 	{
