b3df2eeb7260e2f78da2157a6ceeb29462306576
unknown <vladocrat@gmail.com>
2023-03-27 23:39:25 +0300

    updated handle interface

--- handle.cpp
+++ handle.cpp
@@ -8,11 +8,16 @@ namespace WinUtils
 {
 	Handle::Handle() {}
 
-	Handle::Handle(HANDLE h, std::function<void()> close)
+	Handle::Handle(HANDLE h, const std::function<void()>& close)
 		: m_h(h), m_closeFunc(close) {}
 
 	Handle::~Handle()
 	{
+		if (m_closeFunc)
+		{
+			m_closeFunc();
+		}
+
 		if (m_h == INVALID_HANDLE_VALUE)
 		{
 			std::cerr << "handle is invalid" << std::endl;
@@ -22,20 +27,11 @@ namespace WinUtils
 		if (!CloseHandle(m_h))
 		{
 			std::cerr << "failed to close handle: " << lastErrorAsString() << std::endl;
-
-			if (m_closeFunc)
-			{
-				m_closeFunc();
-			}
-		}
-
-		if (m_closeFunc)
-		{
-			m_closeFunc();
+			return;
 		}
 	}
 
-	void Handle::setFunc(std::function<void()> f)
+	void Handle::setFunc(const std::function<void()>& f)
 	{
 		m_closeFunc = f;
 	}
--- handle.h
+++ handle.h
@@ -5,18 +5,21 @@
 
 namespace WinUtils
 {
-	class Handle
+	class Handle final
 	{
 	public:
 		Handle();
-		Handle(HANDLE h, std::function<void()> close = {});
+		Handle(HANDLE h, const std::function<void()>& close = {});
+		Handle(const Handle&) = delete;
+		Handle(Handle&&) = delete;
+		Handle operator=(const Handle&) = delete;
 		~Handle();
 
-		void setFunc(std::function<void()>);
+		void setFunc(const std::function<void()>&);
 		void setWinHandle(HANDLE h);
-		bool ok() { return m_h != INVALID_HANDLE_VALUE; }
 
-		HANDLE* winHandle() { return &m_h; }
+		[[nodiscard]] bool ok() { return m_h != INVALID_HANDLE_VALUE; }
+		const HANDLE winHandle() const { return m_h; }
 
 	private:
 		HANDLE m_h = INVALID_HANDLE_VALUE;
